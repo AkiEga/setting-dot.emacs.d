e6b4f2aaa59a40269a6b9f4f095472eb1caeddb4
Ernest Lee <fire@chibifire.com>
2011-09-22 06:13:58 -0700

    Changed safe theme.

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("86adc18aa6fb3ea0a801831f7b0bc88ed5999386" default))))
+ '(custom-safe-themes (quote ("f61893043f8c1d7953ad6a7452a9bea63ab27e81" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
