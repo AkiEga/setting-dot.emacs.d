8bf8e48d6d1fed4cbc8686f73d49af16daf8b8e6
Ernest Lee <fire@chibifire.com>
2011-09-22 05:56:34 -0700

    Clean fire.el order.

--- fire.el
+++ fire.el
@@ -1,6 +1,7 @@
 (menu-bar-mode 1)
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (global-visual-line-mode 1)
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 
 (autoload 'markdown-mode "markdown-mode.el" 
   "Major mode for editing Markdown files" t) 
