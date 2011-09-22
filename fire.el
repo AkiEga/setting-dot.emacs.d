9244895c742bf44745afcf2295d1f4396b4fffc8
Ernest Lee <fire@chibifire.com>
2011-09-22 06:03:08 -0700

    Load libraries from site-lisp.

--- fire.el
+++ fire.el
@@ -1,6 +1,14 @@
 (menu-bar-mode 1)
 (global-visual-line-mode 1)
 
+(let ((default-directory "~/.emacs.d/site-lisp/"))
+      (setq load-path
+            (append
+             (let ((load-path (copy-sequence load-path))) ;; Shadow
+               (append 
+                (copy-sequence (normal-top-level-add-to-load-path '(".")))
+                (normal-top-level-add-subdirs-to-load-path)))
+             load-path)))
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 
 (autoload 'markdown-mode "markdown-mode.el" 
