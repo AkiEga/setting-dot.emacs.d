b5646e2c3125de87f20d0d1ecca13d2578a37fda
Ernest Lee <fire@chibifire.com>
2011-09-22 20:49:46 -0700

    Indent code.

--- fire.el
+++ fire.el
@@ -2,13 +2,13 @@
 (global-visual-line-mode 1)
 
 (let ((default-directory "~/.emacs.d/site-lisp/"))
-      (setq load-path
-            (append
-             (let ((load-path (copy-sequence load-path))) ;; Shadow
-               (append 
-                (copy-sequence (normal-top-level-add-to-load-path '(".")))
-                (normal-top-level-add-subdirs-to-load-path)))
-             load-path)))
+  (setq load-path
+        (append
+         (let ((load-path (copy-sequence load-path))) ;; Shadow
+           (append 
+            (copy-sequence (normal-top-level-add-to-load-path '(".")))
+            (normal-top-level-add-subdirs-to-load-path)))
+         load-path)))
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 
 (autoload 'markdown-mode "markdown-mode.el" 
