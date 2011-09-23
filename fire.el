aec1fb01a93261e4b7fc8abee2776841285fffef
Ernest Lee <fire@chibifire.com>
2011-09-22 20:47:45 -0700

    Change markdown extension to md.

--- fire.el
+++ fire.el
@@ -14,4 +14,4 @@
 (autoload 'markdown-mode "markdown-mode.el" 
   "Major mode for editing Markdown files" t) 
 (setq auto-mode-alist 
-  (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
+      (cons '("\\.md" . markdown-mode) auto-mode-alist))
