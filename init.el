be05ab41b88d06724904db00b2ddfdef72fedf5b
Ernest Lee <fire@chibifire.com>
2011-09-23 06:30:38 -0700

    Add themes.

--- init.el
+++ init.el
@@ -7,7 +7,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("2dc8cbf44dbc8532764a313374230ca013b35483" "f61893043f8c1d7953ad6a7452a9bea63ab27e81" default))))
+ '(custom-safe-themes (quote ("2dc8cbf44dbc8532764a313374230ca013b35483" "f61893043f8c1d7953ad6a7452a9bea63ab27e81" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
