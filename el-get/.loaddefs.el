df02008932a42b52f92d7617025541413897e8cc
Ernest Lee <fire@chibifire.com>
2011-11-14 16:06:52 -0800

    Install el-get.

---
+++ el-get/.loaddefs.el
@@ -0,0 +1,20 @@
+;;; .loaddefs.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("auto-complete-clang/auto-complete-clang.el"
+;;;;;;  "auto-complete-css/auto-complete-css.el" "auto-complete-etags/auto-complete-etags.el"
+;;;;;;  "auto-complete/auto-complete-config.el" "auto-complete/auto-complete.el"
+;;;;;;  "auto-complete/fuzzy.el" "auto-complete/popup.el") (20142
+;;;;;;  22811 63275))
+
+;;;***
+
+(provide '.loaddefs)
+;; Local Variables:
+;; version-control: never
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; .loaddefs.el ends here
---
+++ el-get/.status.el
@@ -0,0 +1,1 @@
+(:auto-complete "installed" :auto-complete-clang "installed" :auto-complete-css "installed" :auto-complete-etags "installed")
